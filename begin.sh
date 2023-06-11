974417d39c37071d50660eb96f78797112a1adbf
Logan <ultimate360solutions@gmail.com>
2023-06-11 02:28:26 -0500

    added autostart scripts and adjusted style

---
+++ begin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This is a shell script used to start the program, can be called on OS startup to auto start the program
+# Don't forget to make this file executable with sudo chmod +x begin.sh
+# Change the path below to the path of the program on your machine
+# HINT: Make sure you either save this file from the linux device itself or use a text editor that saves in UNIX format. If this was saved on a windows machine, it will not work!
+cd /home/logan/Desktop/AirConControl-Electron-MINI
+npm start
