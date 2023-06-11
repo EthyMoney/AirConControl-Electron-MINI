4b8f3afe3a8aaae7262fb607aa6d9976dc5fb770
Logan <ultimate360solutions@gmail.com>
2023-06-11 17:10:20 -0500

    changed windows CR notes

--- begin.sh
+++ begin.sh
@@ -2,6 +2,6 @@
 # This is a shell script used to start the program, can be called on OS startup to auto start the program
 # Don't forget to make this file executable with sudo chmod +x begin.sh
 # Change the path below to the path of the program on your machine
-# HINT: Make sure you either save this file from the linux device itself or use a text editor that saves in UNIX format. If this was saved on a windows machine, it will not work!
+# HINT: If you edited this file in Windows, you may need to run this on the file to fix the windows line endings:  sed -i -e 's/\r$//' begin.sh
 cd /home/logan/Desktop/AirConControl-Electron-MINI
 npm start
